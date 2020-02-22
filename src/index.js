736855f1add0584db5789e9bfa4c3aac07702f87
kanjamad bosel <kanjamadpor@gmail.com>
2020-02-22 15:25:20 -0800

    Component Reuse

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return(
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            < ApprovalCard >
+            <div>
+                <h4>Warring!!</h4>
+                Are you sure want to do this?
+            </div>
+            </ApprovalCard >
+
+            < ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM " 
+                    comment=" Nice blog!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            < ApprovalCard >
+                <CommentDetail 
+                    authjor="Alex" 
+                    timeAgo="Today at 2PM " 
+                    comment="Great!!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ ApprovalCard >
+
+            < ApprovalCard >
+                <CommentDetail 
+                    authjor="Por" 
+                    timeAgo="Today at 5PM " 
+                    comment="I love it!!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard >
         </div>
     );
 };
